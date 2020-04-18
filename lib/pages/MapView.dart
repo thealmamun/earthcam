f2103c7b42cdc30e1013c40e78995b90054c6ce5
Al Mamun <mrhmamunbd@gmail.com>
2020-04-18 16:21:55 +0600

    google map initialized

--- lib/pages/MapView.dart
+++ lib/pages/MapView.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapView extends StatefulWidget {
   @override
@@ -10,35 +10,32 @@ class _MapViewState extends State<MapView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text('Map View'),
-          actions: [
-            IconButton(
-                icon: Icon(
-                  Icons.search,
-                  color: Colors.white,
-                  size: 30,
-                ),
-                onPressed: null),
-            IconButton(
-                icon: Icon(
-                  Icons.do_not_disturb_off,
-                  color: Colors.white,
-                  size: 25,
-                ),
-                onPressed: null),
-          ],
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Map View'),
+        actions: [
+          IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+                size: 30,
+              ),
+              onPressed: null),
+          IconButton(
+              icon: Icon(
+                Icons.do_not_disturb_off,
+                color: Colors.white,
+                size: 25,
+              ),
+              onPressed: null),
+        ],
+      ),
+      body: GoogleMap(
+        initialCameraPosition: CameraPosition(
+          target: LatLng(23.7209, 90.4833),
+          zoom: 12,
         ),
-        body: Column(
-          children: [
-            Text('1'),
-            Text('1'),
-            Text('1'),
-            Text('1'),
-            Text('1'),
-          ],
-        )
+      ),
     );
   }
 }
